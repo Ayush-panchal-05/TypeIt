8790f898b41632030a99e91fef86dc00dc4f338e
Ayush Panchal <https://github.com/Ayush-panchal-05/News-App>
2023-09-11 13:15:32 +0530

    TypeIt

---
+++ src/components/layout/footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+        <footer className="footer">
+        <div className="content has-text-centered">
+        <p>
+        <strong>Typeit</strong> by <a href="https://www.linkedin.com/in/ayush-panchal-4812561b6/" target='_blank'>Ayush Panchal</a>
+        </p>
+        </div>
+        </footer>
+    </div>
+  )
+}
